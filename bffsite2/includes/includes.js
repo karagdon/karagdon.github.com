f7c8c88dd5f501ce0b6053ba598f71dd25efc193
Katherine Reyes <karagdon@gmail.com>
2013-06-27 12:46:49 -0700

    Duplicate for visual testing - Margins
    Sidenav
    Imagebar

---
+++ bffsite2/includes/includes.js
@@ -0,0 +1,16 @@
+    $(function(){
+		$("#popcontent").load("includes/popcontent.html");      
+		$("#headercontent").load("includes/header.html"); 
+		$("#indexcontent").load("includes/index_content.html");
+					$("#faqcontent").load("includes/faq.html");
+					// $("#projcontent").load("includes/aboutproj.html");
+					// $("#permacontent").load("includes/permaculture.html");					
+		$("#imgcontent").load("includes/imageBar.html");
+		$("#sidecontent").load("includes/sidenav.html");
+		$("#footercontent").load("includes/footer.html");
+		
+    });
+	
+
+	
+	
