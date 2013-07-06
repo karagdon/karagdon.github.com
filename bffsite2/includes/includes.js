0e7056cc77708585ecc7f702039712eb037e4780
Katherine Reyes <karagdon@gmail.com>
2013-07-05 19:17:20 -0700

    Revert "Lots of Updates"
    This reverts commit 352427e0c698e7647f60e91ff36bad71bf1b310d.

--- bffsite2/includes/includes.js
+++ bffsite2/includes/includes.js
@@ -2,10 +2,9 @@
 		$("#popcontent").load("includes/popcontent.html");      
 		$("#headercontent").load("includes/header.html"); 
 		$("#indexcontent").load("includes/index_content.html");
-					$("#faqcontent").load("includes/faq_content.html");
-					$("#projcontent").load("includes/project_content.html");
-					$("#startcontent").load("includes/start_content.html");
-					$("#permacontent").load("includes/permaculture_c.html");					
+					$("#faqcontent").load("includes/faq.html");
+					// $("#projcontent").load("includes/aboutproj.html");
+					// $("#permacontent").load("includes/permaculture.html");					
 		$("#imgcontent").load("includes/imageBar.html");
 		$("#sidecontent").load("includes/sidenav.html");
 		$("#footercontent").load("includes/footer.html");
